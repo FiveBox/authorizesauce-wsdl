ff67a1c4638ab698989bcd299055278013a35800
davejmac <david.mcnamara@outlook.com>
2016-03-24 17:39:45 -0400

    set default not to False

--- authorize/conf.py
+++ authorize/conf.py
@@ -4,7 +4,7 @@ from appconf import AppConf
 
 class authorizeConf(AppConf):
 
-    TEST_URL = False
+    TEST_URL = 'https://apitest.authorize.net/soap/v1/Service.asmx?WSDL'
 
     class Meta:
         prefix = 'authorize'
